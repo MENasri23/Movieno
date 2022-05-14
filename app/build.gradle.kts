57157fcd02894e0a9d4a86b96bf0f2c252a3f841
MENasri23 <m.e26159487@gmail.com>
2022-05-14 05:30:56 -0700

    Migrate to kotlin build kts and refactor code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,19 @@
+import Libs.Androidx.implementAppLibraries
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
-    compileSdk = 32
+    compileSdk = App.COMPILE_SDK
 
     defaultConfig {
-        applicationId = "ir.jatlin.movieno"
-        minSdk = 21
-        targetSdk = 32
-        versionCode = 1
-        versionName = "1.0"
+        applicationId = App.APPLICATION_ID
+        minSdk = App.MIN_SDK
+        targetSdk = App.TARGET_SDK
+        versionCode = App.VERSION_CODE
+        versionName = App.versionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -36,10 +38,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementAppLibraries()
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
