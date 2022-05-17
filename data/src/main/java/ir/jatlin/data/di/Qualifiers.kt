f1413730cb445b40bde6ef6491a16a9b76260c47
MENasri23 <m.e26159487@gmail.com>
2022-05-17 04:39:59 -0700

    Add dispatcher to graph

---
+++ data/src/main/java/ir/jatlin/data/di/Qualifiers.kt
@@ -0,0 +1,16 @@
+package ir.jatlin.data.di
+
+import javax.inject.Qualifier
+
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IODispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class MainDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class CpuDispatcher
