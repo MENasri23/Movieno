c857ca05deff5bd529853ae97c0747b45f4a878c
MENasri23 <m.e26159487@gmail.com>
2022-05-18 03:52:09 -0700

    Implement movie remote data source with api caller

--- data/src/main/java/ir/jatlin/data/source/remote/MovieRetrofitDataSource.kt
+++ data/src/main/java/ir/jatlin/data/source/remote/MovieRetrofitDataSource.kt
@@ -2,6 +2,7 @@ package ir.jatlin.data.source.remote
 
 import ir.jatlin.core.data.source.MovieRemoteDataSource
 import ir.jatlin.webservice.api.MovieApi
+import ir.jatlin.webservice.api.SafeApiCall
 import ir.jatlin.webservice.model.movie.MovieDetailsDTO
 import ir.jatlin.webservice.model.response.MoviesResponse
 import ir.jatlin.webservice.model.response.UpcomingMoviesResponse
@@ -10,23 +11,24 @@ import javax.inject.Singleton
 
 @Singleton
 class MovieRetrofitDataSource @Inject constructor(
-    private val networkApi: MovieApi
+    private val networkApi: MovieApi,
+    private val safeCall: SafeApiCall
 ) : MovieRemoteDataSource {
 
     override suspend fun getMovieDetails(movieId: Long): MovieDetailsDTO {
-        return networkApi.getMovieDetails(movieId)
+        return safeCall { networkApi.getMovieDetails(movieId) }
     }
 
     override suspend fun getPopulars(page: Int): MoviesResponse {
-        return networkApi.getPopulars(page)
+        return safeCall { networkApi.getPopulars(page) }
     }
 
     override suspend fun getUpcoming(page: Int): UpcomingMoviesResponse {
-        return networkApi.getUpcoming(page)
+        return safeCall { networkApi.getUpcoming(page) }
     }
 
     override suspend fun getTopRated(page: Int): MoviesResponse {
-        return networkApi.getTopRated(page)
+        return safeCall { networkApi.getTopRated(page) }
     }
 
     override suspend fun discoverMovies(
@@ -34,8 +36,11 @@ class MovieRetrofitDataSource @Inject constructor(
         sortBy: String?,
         filters: Map<String, String>?
     ): MoviesResponse {
-        return networkApi.discoverMovies(
-            page = page, sortBy = sortBy, filters = filters
-        )
+        return safeCall {
+            networkApi.discoverMovies(
+                page = page, sortBy = sortBy, filters = filters
+            )
+        }
     }
+
 }
