4e1b7bf6c87cab9f17e76315df59ff4e9ceb4155
MENasri23 <m.e26159487@gmail.com>
2022-05-18 19:34:26 -0700

    Define use case for fetching genres

---
+++ domain/src/main/java/ir/jatlin/domain/movie/GetAllGenres.kt
@@ -0,0 +1,13 @@
+package ir.jatlin.domain.movie
+
+import ir.jatlin.core.data.repository.GenreRepository
+import ir.jatlin.model.movie.Genre
+import javax.inject.Inject
+
+class GetAllGenres @Inject constructor(
+    private val genreRepository: GenreRepository
+) {
+
+    suspend operator fun invoke(): List<Genre> =
+        genreRepository.getAllGenres()
+}
