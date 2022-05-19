9b1b55cb9f100199cc887ed0ba1a5ffa10f7c7d7
MENasri23 <m.e26159487@gmail.com>
2022-05-18 21:33:17 -0700

    Define use case to get up coming movies

---
+++ domain/src/main/java/ir/jatlin/domain/movie/GetUpcomingMoviesUesCase.kt
@@ -0,0 +1,17 @@
+package ir.jatlin.domain.movie
+
+import ir.jatlin.core.data.repository.MovieRepository
+import ir.jatlin.domain.FlowUseCase
+import ir.jatlin.model.info.MoviesInfo
+import ir.jatlin.model.info.UpcomingMoviesInfo
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetUpcomingMoviesUesCase @Inject constructor(
+    private val movieRepository: MovieRepository
+) : FlowUseCase<Int, UpcomingMoviesInfo>() {
+
+    override fun execute(params: Int): Flow<UpcomingMoviesInfo> {
+        return movieRepository.getUpcoming(page = params)
+    }
+}
